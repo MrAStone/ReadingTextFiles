961ee028333738721d9cd9ea5a7cd9fe915a95b2
A Stone <astone@heckgrammar.co.uk>
2022-12-05 14:34:37 +0000

    Add project files.

---
+++ ReadingTextFiles/Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Dim FileToRead As New StreamReader("U:\advent22\day1testdata.txt")
+        ' read it all
+        '    Console.WriteLine(FileToRead.ReadToEnd)
+
+        ' read it all and save in a string
+        '       Dim fileContents As String = FileToRead.ReadToEnd
+        '      Console.WriteLine(fileContents)
+
+        'read line by line
+        '        Do
+        '        Console.WriteLine(FileToRead.ReadLine)
+        '        Loop Until FileToRead.EndOfStream
+
+        'read line by line and store in a list
+        Dim fileLines As New List(Of String)
+        Do
+            fileLines.Add(FileToRead.ReadLine)
+        Loop
+        Console.WriteLine(fileLines(0)) ' the first line in the text file
+    End Sub
+
+End Module
